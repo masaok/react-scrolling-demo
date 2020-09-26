e5d90fd2425af9b25e2300177516e89ffbbd677a
Masao Kitamura <masaok@gmail.com>
2020-09-26 02:44:53 -0700

    Fixed scrolling container that flexes with browser

--- src/components/ScrollInsideContainer.js
+++ src/components/ScrollInsideContainer.js
@@ -3,46 +3,38 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 
-// import logo from './logo.svg'
 import { Helmet } from 'react-helmet'
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    color: theme.palette.common.white,
-    backgroundColor: 'blue',
+    // color: theme.palette.common.white,
+    // backgroundColor: 'blue',
     textAlign: 'center',
     fontSize: 20,
     alignItems: 'center',
     overflow: 'auto',
-    maxHeight: 500,
+    // maxHeight: 500,
     minWidth: '50vw',
-    margin: theme.spacing(3),
-  },
-  appBar: {
-    top: 0,
-  },
-  paper: {
-    margin: theme.spacing(1),
-    padding: theme.spacing(1),
+    margin: theme.spacing(5),
   },
 }))
 
 const rows = []
 for (var i = 1; i <= 50; i++) {
-  rows.push(<div>test vertical scroll {i} inside container</div>)
+  rows.push(<div>test vertical scroll {i} inside container that flexes with browser resizing</div>)
 }
 
 const PaperScrollDemo = props => {
   const classes = useStyles(props)
   return (
-    <div className={classes.root}>
+    <Paper className={classes.root}>
       <Helmet>
         <title>Scroll Inside Container</title>
       </Helmet>
-      <div className={classes.container}>{rows}</div>
-    </div>
+      {rows}
+    </Paper>
   )
 }
 
